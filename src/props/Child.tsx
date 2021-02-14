534592ee37901f333bd51525302aaf203d791feb
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-02-13 22:24:16 -0700

    Update components

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,16 +1,35 @@
 interface ChildProps {
   color: string;
+  onClick: () => void;
 }
 
-export const Child = ({ color }: ChildProps) => {
-  return <div>{color}</div>;
+// associate ChildProps interface with this component
+// this way of writing the component it is not able to receive children
+// by default
+export const Child = ({ color, onClick }: ChildProps) => {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
 };
 
 // tell typescript
 // this component is a React Function Component
 // this component might have properties assigned to it like 'propTypes'
 // this component will receive props of type 'ChildProps'
-export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
-  return <div>{color}</div>;
+export const ChildAsFC: React.FC<ChildProps> = ({
+  color,
+  onClick,
+  children,
+}) => {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}>Click me</button>
+      {children}
+    </div>
+  );
 };
 // can use ChildAsFC.displayName etc and other React component methods
--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -1,7 +1,13 @@
-import { Child } from "./Child";
+import { ChildAsFC } from "./Child";
 
+// everything inside the component tags will br provided to the Child component
+// as a props called children (this is a feature in React)
 const Parent = () => {
-  return <Child color="red" />;
+  return (
+    <ChildAsFC color="red" onClick={() => console.log("click")}>
+      This text will show up as children in ChildAsFC component
+    </ChildAsFC>
+  );
 };
 
 export default Parent;
